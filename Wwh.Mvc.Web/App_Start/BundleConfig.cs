cf66af76eacbf3a1323781795c9490a724846088
吴卫华 <wfnghwxf@outlook.com>
2017-10-26 21:22:53 +0800

    第一次测试index.cshtml失败

--- Wwh.Mvc.Web/App_Start/BundleConfig.cs
+++ Wwh.Mvc.Web/App_Start/BundleConfig.cs
@@ -9,10 +9,18 @@ namespace Wwh.MVC.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/easyui").Include(
+                        "~/Scripts/jquery.easyui-{version}.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/home").Include(
+                       "~/Scripts/home.js"));
+
+            //easyui
+            bundles.Add(new StyleBundle("~/Content/themes/blue/css").Include("~/Content/themes/blue/easyui.css"));
+            bundles.Add(new StyleBundle("~/Content/themes/gray/css").Include("~/Content/themes/gray/easyui.css"));
+            bundles.Add(new StyleBundle("~/Content/themes/metro/css").Include("~/Content/themes/metro/easyui.css"));
 
             // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
             // 生产准备就绪，请使用 https://modernizr.com 上的生成工具仅选择所需的测试。
